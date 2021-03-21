85537a653d732466d44f2fcfb9409dedd3267bd1
sn0wfree <snowfreedom0815@gmail.com>
2021-03-21 16:41:44 +0800

    init
    init

---
+++ QuantOPT/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+import pandas as pd
+import numpy as np
+__version__ = '0.1'
+__author__ = 'sn0wfree'
+if __name__ == '__main__':
+    pass
+
+
---
+++ QuantOPT/conf/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+
+class CONF(object):
+    constraints = 'set.yaml'
+    pass
+
+
+if __name__ == '__main__':
+    pass
